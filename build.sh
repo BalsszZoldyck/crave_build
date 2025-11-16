5f5899812840a3f1483b06ffa4d91e90dfb24309
Balsz <filecloudrom@gmail.com>
2025-11-16 22:21:19 +0700

    BARUDAK PHONGE TERORRSS

--- build.sh
+++ build.sh
@@ -1,1 +1,41 @@
+rm -rf .repo/local_manifests/
+rm -rf out/soong out/host/linux-x86
+rm -rf device/asus/sdm660-common
+rm -rf vendor/asus
 
+# Symlink libncurses 6 >> 5
+sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6   /usr/lib/x86_64-linux-gnu/libtinfo.so.5
+echo "============="
+echo "lib6 >> lib5  "
+echo "============="
+
+#repo init
+repo init -u https://github.com/Havoc-OS-Revived/android_manifest.git -b eleven --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+#local_manifest
+https://github.com/BalsszZoldyck/local_manifests -b Havoc .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+#Sync
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=Bee Zoldcyk
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+. build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+#build
+lunch havoc_X00TD-userdebug && make installclean && m bacon
